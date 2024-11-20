178488f964d06314fabd0e101c3278e22998866e
SrIzan <izan.garciag@gmail.com>
2024-11-20 14:48:06 +0100

    fix: solucionado error al parsear de un documento xml a json

--- wrappers/cle_extractor.py
+++ wrappers/cle_extractor.py
@@ -1,16 +1,37 @@
-# TODO: Implement
 import pandas as pd
-from lxml import etree
+import xml.etree.ElementTree as ET
 
+# Wrapper: Read XML and convert to JSON
+tree = ET.parse("wrappers/data_sources/monumentos.xml")
+root = tree.getroot()
 
-#wrapper
-df = pd.read_xml("wrappers\data\monumentos.xml", xpath = ".//monumento")
+data = []
+for monumento in root.findall(".//monumento"):
+    item = {}
+    for child in monumento:
+        if child.tag == 'coordenadas' or child.tag == 'poblacion':
+            item[child.tag] = {subchild.tag: subchild.text for subchild in child}
+        else:
+            item[child.tag] = child.text
+    data.append(item)
+
+df = pd.DataFrame(data)
 json = df.to_json(orient="records", indent=4)
 
-#extractor
+# Extractor: Read JSON
 df_json = pd.read_json(json)
 
-print(df_json['calle'])
+monumento_nombres = df_json['nombre']
+monumento_tipos = df_json['tipoMonumento']
+monumento_direcciones = df_json['calle']
+monumento_codigos_postales = df_json['codigoPostal']
+
+monumento_longitudes = df_json['coordenadas'].apply(lambda x: x['longitud'])
+monumento_latitudes = df_json['coordenadas'].apply(lambda x: x['latitud'])
+# localidad_nombres = df_json['poblacion'].apply(lambda x: x['localidad'])
+# poblacion_nombres = df_json['poblacion'].apply(lambda x: x['provincia'])
+
+print(monumento_latitudes)
 
 #for nombre, tipoMonumento, calle in zip(df_json['nombre'], df_json['tipoMonumento'], df_json['calle']):
  #   print(f"Nombre: {nombre}, tipoMonumento: {tipoMonumento}, calle: {calle}")
