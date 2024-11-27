1d9cf66a6e0f5e82f2e4f070fc2cb6e93a48f388
SrIzan <izan.garciag@gmail.com>
2024-11-27 13:29:44 +0100

    Feat: implemented extractor consts

--- wrappers/cle_extractor.py
+++ wrappers/cle_extractor.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 
+from models.models import Tipo
+
 # Wrapper: Read XML and convert to JSON
 tree = ET.parse("wrappers/data_sources/monumentos.xml")
 root = tree.getroot()
@@ -28,10 +30,37 @@ monumento_codigos_postales = df_json['codigoPostal']
 
 monumento_longitudes = df_json['coordenadas'].apply(lambda x: x['longitud'])
 monumento_latitudes = df_json['coordenadas'].apply(lambda x: x['latitud'])
-# localidad_nombres = df_json['poblacion'].apply(lambda x: x['localidad'])
+localidad_nombres = df_json['poblacion'].apply(lambda x: x['localidad'] if x is not None else '')
+provincia_nombres = df_json['poblacion'].apply(lambda x: x['provincia'] if x is not None else '')
 # poblacion_nombres = df_json['poblacion'].apply(lambda x: x['provincia'])
 
-print(monumento_latitudes)
+print(monumento_tipos)
+
+# Define a mapping that converts the monumento_tipos to the Tipo enum
+tipo_mapping = {
+    'Yacimientos arqueológicos': Tipo.YACIMIENTO_ARQUEOLOGICO,
+
+    'Iglesias y Ermitas': Tipo.IGLESIA_ERMITA,
+    'Santuarios': Tipo.IGLESIA_ERMITA,
+    'Catedrales': Tipo.IGLESIA_ERMITA,
+
+    'Monasterios': Tipo.MONASTERIO_CONVENTO,
+
+    'Castillos': Tipo.CASTILLO_FORTALEZA_TORRE,
+    'Torres': Tipo.CASTILLO_FORTALEZA_TORRE,
+    'Murallas y puertas': Tipo.CASTILLO_FORTALEZA_TORRE,
+
+    'Casas Consistoriales': Tipo.EDIFICIO_SINGULAR,
+    'Casas nobles': Tipo.EDIFICIO_SINGULAR,
+    'Palacios': Tipo.EDIFICIO_SINGULAR,
+    'Reales Sitios': Tipo.EDIFICIO_SINGULAR,
+    'Casas Consistoriales': Tipo.EDIFICIO_SINGULAR,
+
+    'puente': Tipo.PUENTE,
+
+    'otros': Tipo.OTROS
+}
+
 
 #for nombre, tipoMonumento, calle in zip(df_json['nombre'], df_json['tipoMonumento'], df_json['calle']):
  #   print(f"Nombre: {nombre}, tipoMonumento: {tipoMonumento}, calle: {calle}")
