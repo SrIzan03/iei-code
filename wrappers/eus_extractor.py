0cf66f5b2fdf843cd99b83278563ad62544489c4
DFERARC <dferarc@etsinf.upv.es>
2024-11-30 18:22:42 +0100

    first version eus extractor

--- wrappers/eus_extractor.py
+++ wrappers/eus_extractor.py
@@ -1,1 +1,97 @@
-# TODO: Implement
+import pandas as pd
+from models import Tipo
+import json
+import numpy
+
+tipo_mapping = {
+    "Zona arqueológica": Tipo.YACIMIENTO_ARQUEOLOGICO,
+    "Yacimiento": Tipo.YACIMIENTO_ARQUEOLOGICO,
+
+    "Iglesia": Tipo.IGLESIA_ERMITA,
+    "Catedral": Tipo.IGLESIA_ERMITA,
+    "Parroquia": Tipo.IGLESIA_ERMITA,
+    "Basílica": Tipo.IGLESIA_ERMITA,
+    "Ermita": Tipo.IGLESIA_ERMITA,
+
+    "Santuario": Tipo.MONASTERIO_CONVENTO,
+    "Monasterio": Tipo.MONASTERIO_CONVENTO,
+    "Convento": Tipo.MONASTERIO_CONVENTO,
+
+    "Fuerte": Tipo.CASTILLO_FORTALEZA_TORRE,
+    "Castillo": Tipo.CASTILLO_FORTALEZA_TORRE,
+    "Torre": Tipo.CASTILLO_FORTALEZA_TORRE,
+    "Muralla": Tipo.CASTILLO_FORTALEZA_TORRE,
+
+    "Villa": Tipo.EDIFICIO_SINGULAR,
+    "Casa Consistorial": Tipo.EDIFICIO_SINGULAR,
+    "Palacio": Tipo.EDIFICIO_SINGULAR,
+    "Ayuntamiento": Tipo.EDIFICIO_SINGULAR,
+
+    "Puente": Tipo.PUENTE,
+
+    "otros": Tipo.OTROS
+}
+
+def determinar_tipo(nombre, tipo_mapping):
+    for palabra_clave, tipo in tipo_mapping.items():
+        if palabra_clave.lower() in nombre.lower():
+            return tipo
+    return Tipo.OTROS
+
+def check_direccion(direccion: str):
+    if direccion is numpy.nan: 
+        # Llamar al servicio que tiene la API para obtener la dirección a través de las coordenadas.
+        return "dirección vacía"
+    else:
+        return direccion
+    
+
+# Wrapper : In the future the wrapper will return all the json data to the extractor instead of the extractor reading it directly.
+DATA_ROUTE = "wrappers/data_sources/edificios2.json"
+
+# Extractor: Read json data from the wrapper and filters the corresponding data to add them to the DB.
+
+df = pd.read_json(DATA_ROUTE)
+
+monumento_nombres = df["documentName"]
+monumento_tipos = df["documentName"].apply(lambda x: determinar_tipo(x, tipo_mapping))
+monumento_direcciones = df["address"].fillna("").astype(str)
+monumento_codigos_postales = df["postalCode"].fillna("").astype(str)
+monumento_longitudes = df["lonwgs84"]
+monumento_latitudes = df["latwgs84"]
+monumento_descripciones = df["documentDescription"]
+
+localidad_nombres = df["municipality"].fillna("").astype(str)
+
+provincia_nombres = df["territory"]
+
+def pass_data_to_service():
+    from models import MonumentoCreate, LocalidadCreate, ProvinciaCreate
+    for i in range(len(monumento_nombres)):
+        MonumentoCreate(
+            monumento_nombres[i],
+            monumento_tipos[i],
+            monumento_direcciones[i],
+            monumento_codigos_postales[i],
+            monumento_longitudes[i],
+            monumento_latitudes[i],
+            monumento_descripciones[i],
+        )
+        LocalidadCreate(
+            localidad_nombres[i],
+        )
+        ProvinciaCreate(
+            provincia_nombres[i],
+        )
+
+
+def print_codigo():
+    with open('resultados.txt', 'w', encoding='UTF-8') as file:
+        for i in range(provincia_nombres.size):
+            file.write(str(provincia_nombres[i]) + '\n')
+
+    print(monumento_direcciones[38])
+
+
+
+
