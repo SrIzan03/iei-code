de56ee56ee2ec7349d8b9590dcea073e281c481a
caarlosdiaz <132602821+caarlosdiaz@users.noreply.github.com>
2024-11-30 20:10:23 +0100

    feat: implemented cv extractor

--- wrappers/cv_extractor.py
+++ wrappers/cv_extractor.py
@@ -1,48 +1,52 @@
 import pandas as pd
-import requests
-from models.models import Tipo
+from models import Tipo, MonumentoCreate, ProvinciaCreate, LocalidadCreate
+from services import get_direccion_and_cod_postal, utm_to_lat_long
 
 def wrap() -> str:
     df = pd.read_csv('./wrappers/data_sources/bienes_inmuebles_interes_cultural.csv', delimiter=";")
     return df.to_json()
 
 def getTipo(denominacion: str, codCategoria: int):
+    denominacion = denominacion.lower()
+
     if codCategoria in [5, 7]:
         return Tipo.YACIMIENTO_ARQUEOLOGICO
-    if denominacion in ["Iglesia", "Ermita"]:
+    if any(keyword in denominacion for keyword in ["iglesia", "ermita"]):
         return Tipo.IGLESIA_ERMITA
-    if denominacion in ["Monasterio", "Convento"]:
+    if any(keyword in denominacion for keyword in ["monasterio", "convento"]):
         return Tipo.MONASTERIO_CONVENTO
-    if denominacion in ["Castillo", "Fortaleza", "Torre"]:
+    if any(keyword in denominacion for keyword in ["castillo", "fortaleza", "torre"]):
         return Tipo.CASTILLO_FORTALEZA_TORRE
     if codCategoria in [11]:
         return Tipo.EDIFICIO_SINGULAR
-    if denominacion in ["Puente"]:
+    if "puente" in denominacion:
         return Tipo.PUENTE
     return Tipo.OTROS
 
 # TODO: refactor, in the future we will not pass a str
 def extract(json: str):
     df = pd.read_json(json)
-    # CV['IGPCV', 'DENOMINACION', 'PROVINCIA', 'MUNICIPIO', 'UTMESTE', 'UTMNORTE', 'CODCLASIFICACION', 'CLASIFICACION', 'CODCATEGORIA', 'CATEGORIA']
-    for tuple in df.itertuples():
-        response = utm_to_lat_long(tuple['UTMNORTE'],tuple['UTMESTE'])
-        denominacion = tuple['DENOMINACION']
-        codCategoria = tuple['CODCATEGORIA']
 
-        nombre = denominacion
+    for _, row in df.iterrows():
+        response = utm_to_lat_long(row['UTMNORTE'],row['UTMESTE'])
+        denominacion = row['DENOMINACION']
+        codCategoria = row['CODCATEGORIA']
+
         tipo = getTipo(denominacion, codCategoria)
         latitude = response.get('latitude')
         longitude = response.get('longitude')
-        
+        direccion, postCode = get_direccion_and_cod_postal(latitude, longitude)
 
-def utm_to_lat_long(utm_north: str, utm_east:str):
-    BOT_URL = "http://iei-bot:8000/utm-to-lat-long/utm_north/utm_east"
-    response = requests.get(BOT_URL, params={"utm_north": utm_north, "utm_east": utm_east})
-    return response.json()
+        monumento = MonumentoCreate(
+            denominacion,
+            tipo,
+            direccion,
+            postCode,
+            longitude,
+            latitude,
+            ''
+        )
 
+        localidad = LocalidadCreate(row['MUNICIPIO'])
 
-
-
-json = wrap()
-extract(json)
+        provincia = ProvinciaCreate(row['PROVINCIA'])
