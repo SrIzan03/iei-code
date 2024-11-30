9ffd7b3c485cc4db777f1acc226e92b30ca245b1
caarlosdiaz <132602821+caarlosdiaz@users.noreply.github.com>
2024-11-30 20:08:46 +0100

    chore: updated modules

--- wrappers/__init__.py
+++ wrappers/__init__.py
@@ -1,3 +1,4 @@
 from .cle_extractor import get_tipo, print_example, pass_data_to_service
+from .cv_extractor import wrap, extract
 
-__all__ = ['get_tipo', 'print_example', 'pass_data_to_service']
+__all__ = ['get_tipo', 'print_example', 'wrap', 'extract', 'pass_data_to_service']
